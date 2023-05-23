3105905cf5687ccda849154cec0a71802418d89f
kurasiak <kurasiak.yoh.pat.ger@outlook.fr>
2023-05-23 10:56:26 +0200

    create react and java link
    23/05/23023 10:55 start of creation react and java link

--- src/main/java/fr/dawan/projetcompagnieaerienne/controllers/ReservationController.java
+++ src/main/java/fr/dawan/projetcompagnieaerienne/controllers/ReservationController.java
@@ -1,9 +1,11 @@
 package fr.dawan.projetcompagnieaerienne.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 @RequestMapping("")
 public class ReservationController {
 
